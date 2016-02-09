005561516b3edfeff20110e11a1fa2eddbc1185d
Connor Moore <moorec22@cs.washington.edu>
2016-02-08 22:12:34 -0800

    Parsed data into a file, parsed into variables in cluster

--- models/cluster.py
+++ models/cluster.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+import numpy as np
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets.samples_generator import make_blobs
+
 # Clustering
 
 import sys
@@ -14,3 +18,12 @@ def execute(args):
   if len(args) < 1:
     usage()
     sys.exit()
+  y, x = parse(args[0])
+
+# Parses the given file into a matrix of data. The depenedent variable is assumed
+# to be at the beginning
+def parse(filename):
+  raw = [[feature for feature in line.strip().split(',')] for line in open(filename, 'r')]
+  dependent = [sample[0] for sample in raw]
+  independent = [sample[1:] for sample in raw]
+  return dependent, independent
